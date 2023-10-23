d4e585f98bcad679c45d720e1a0e799f113c556e
Evan Graham <evangraham19@gmail.com>
2023-10-23 15:08:36 -0700

    music 2

---
+++ models/band.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
+    band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    // todo: evan finish updating the remaining fields
+    genre: DataTypes.TEXT,
+    available_start_time: DataTypes.DATE,
+    end_time: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Band',
+    //todo:: evan -- add tableName and timestamps
+  });
+  return Band;
+};
